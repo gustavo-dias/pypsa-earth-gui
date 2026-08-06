3f86da73189a25fc5a2c8947d1f375eb19a1f0e8
Gustavo Dias <gustavodias.po@gmail.com>
2026-08-06 13:07:08 +0200

    Add Literal annotation to enforce valid values

--- app/constants.py
+++ app/constants.py
@@ -13,13 +13,15 @@ FOLDER_PROMPT_TITLE \\
 ERROR_MSG
 """
 
+from typing import Literal
+
 from app.config.constants import CONFIG_FILES_EXTENSION
 
 ###### GENERAL ######
 BASE_DIR: str = 'app'
 LOGO_PATH: str = f'./{BASE_DIR}/static/logo.png'
 LOGO_LINK_URL: str = 'https://pypsa-meets-earth.github.io/'
-LOGO_SIZE: str = 'large'
+LOGO_SIZE: Literal['small', 'medium', 'large'] = 'large'
 
 ###### ENTRY VIEW CONSTANTS ######
 HEADER_DIVIDER: str = 'blue' # https://docs.streamlit.io/develop/api-reference/text/st.header
